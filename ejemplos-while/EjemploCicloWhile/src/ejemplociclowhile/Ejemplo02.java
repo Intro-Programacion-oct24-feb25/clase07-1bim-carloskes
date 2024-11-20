2d5983f832ae22f17894838bbea3268ae2e7e9d0
carloskes <cscorrea@utpl.edu.ec>
2024-11-20 12:05:17 -0500

    actualizado

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo02.java
@@ -17,14 +17,16 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
-        entrada.useLocale(Locale.US);
+        entrada.useLocale(Locale.US); // libreria que facilita la
+        //compatibilidad del punto y la coma.
         int limite;
         int contador = 1;
         System.out.println("Ingrese el Límite");
         limite = entrada.nextInt(); 
         while (contador <= limite){
             
-            if (contador%2==0) {
+            if (contador%2==0) { //nos sirve para encontrar un residuo y
+// separar los números pares e impares
                 System.out.printf("Contador %d\n", 
                     contador);
             }
